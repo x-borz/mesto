6035a3758548506d49a48bd9527311bc55a0e871
Renat Kabanov <x-borz@yandex.ru>
2022-03-10 12:57:06 +0300

    feat: Добавить секцию popup и js-код открытия/закрытия попапа, обновления информации о пользователе

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let popup = document.querySelector('.popup');
+let formElement = popup.querySelector(".edit-form");
+let nameInput = formElement.querySelector(".edit-form__item_el_name");
+let jobInput = formElement.querySelector(".edit-form__item_el_job");
+
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let profileNameElement = document.querySelector('.profile__name');
+let profileJobElement = document.querySelector('.profile__job');
+
+function openPopup() {
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileJobElement.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+  profileNameElement.textContent = nameValue;
+  profileJobElement.textContent = jobValue;
+
+  closePopup();
+}
+
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
