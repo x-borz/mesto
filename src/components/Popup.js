81c8edb675774e417e32da763778abfe6594841a
Renat Kabanov <x-borz@yandex.ru>
2022-05-12 16:02:30 +0300

    fix: пофиксить проблему с удалением слушателя нажатия на Escape

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,22 +1,21 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
-  }
-
-  _handleEscClose(evt) {
-    if (evt.key === 'Escape') {
-      this.close();
+    this._handleEscClose = (evt) => {
+      if (evt.key === 'Escape') {
+        this.close();
+      }
     }
   }
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   setEventListeners() {
